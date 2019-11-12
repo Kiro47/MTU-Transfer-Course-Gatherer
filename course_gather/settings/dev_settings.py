c894df7fded1209e7412247d0d7e642fcf3cc18c
blu3spirits <blu3spirits@gmail.com>
2019-11-11 20:32:25 -0500

    Ignore F401, F403 PEP8 errors
    Ignoring these errors because in order for the settings to be structured
    like this the import * is absolutely needed.

--- course_gather/settings/dev_settings.py
+++ course_gather/settings/dev_settings.py
@@ -1,3 +1,3 @@
-from course_gather.settings.base import *
-SECRET_KEY = ''
+from course_gather.settings.base import *  # noqa: F401, F403
+SECRET_KEY = 'your_dev_secret_key_here'
 DEBUG = True
--- course_gather/settings/local_settings.py
+++ course_gather/settings/local_settings.py
@@ -1,6 +1,6 @@
-from course_gather.settings.base import *
+from course_gather.settings.base import *  # noqa: F401, F403
 
-SECRET_KEY = ''
+SECRET_KEY = 'your_local_key_here'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
