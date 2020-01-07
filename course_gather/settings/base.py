5be6d808ac6535496e095b1f6b4e71fd4c244ab5
blu3spirits <blu3spirits@gmail.com>
2020-01-06 20:11:28 -0500

    Address issue #8, add django filters
    Django filters allow us to narrow down our results from the back end.
    For example, instead of doing a GET to /api/colleges/. Do a GET to
    /api/colleges/?college_name=Indiana

--- course_gather/settings/base.py
+++ course_gather/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'course_gather'
 ]
 
@@ -53,6 +54,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffset\
 Pagination',
     'PAGE_SIZE': 100
--- course_gather/viewsets.py
+++ course_gather/viewsets.py
@@ -12,24 +12,40 @@ from course_gather.serializers import (
     StateSerializer
 )
 
+from course_gather.filters import (
+    CollegeFilter,
+    CourseFilter,
+    MTUCourseFilter,
+    StateFilter
+)
+
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
 
 
 class StateViewSet(viewsets.ModelViewSet):
     serializer_class = StateSerializer
     queryset = State.objects.all()
+    filterset_class = StateFilter
+    filter_backends = (filters.DjangoFilterBackend,)
 
 
 class CollegeViewSet(viewsets.ModelViewSet):
     serializer_class = CollegeSerializer
     queryset = College.objects.all()
+    filterset_class = CollegeFilter
+    filter_backends = (filters.DjangoFilterBackend,)
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
+    filterset_class = CourseFilter
+    filter_backends = (filters.DjangoFilterBackend,)
 
 
 class MTUCourseViewSet(viewsets.ModelViewSet):
     serializer_class = MTUCourseSerializer
     queryset = MTUCourse.objects.all()
+    filterset_class = MTUCourseFilter
+    filter_backends = (filters.DjangoFilterBackend,)
