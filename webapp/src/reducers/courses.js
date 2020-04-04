d4e05f561fda225c83723b6c0d990d84fc2f0cfa
blu3spirits <blu3spirits@gmail.com>
2020-04-03 20:20:49 -0500

    Add webapp baseline

---
+++ webapp/src/reducers/courses.js
@@ -0,0 +1,31 @@
+import {
+  GET_COURSES_REQUEST,
+  GET_COURSES_SUCCESS,
+  GET_COURSES_ERROR
+} from '../actions/courses'
+
+const initialState = {
+  loading: false
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_COURSES_REQUEST:
+      return { ...state, loading: true }
+    case GET_COURSES_SUCCESS:
+      let results = action.payload.results;
+      return {
+        ...state,
+        results,
+        loading: false
+      }
+    case GET_COURSES_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      }
+  default:
+    return state
+  }
+}
