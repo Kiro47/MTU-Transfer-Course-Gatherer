b25d5049e976d719b8dff5fdcbb1554fdbb9910c
Max Isom <code@maxisom.me>
2020-04-25 13:12:13 -0500

    Add env var to control API endpoint

--- webapp/src/store/actions/courses.js
+++ webapp/src/store/actions/courses.js
@@ -1,11 +1,12 @@
 import {createAction} from 'redux-api-middleware';
+import {ENDPOINT} from '../../config';
 
 export const GET_COURSES_REQUEST = 'GET_COURSES_REQUEST';
 export const GET_COURSES_SUCCESS = 'GET_COURSES_SUCCESS';
 export const GET_COURSES_ERROR = 'GET_COURSES_ERROR';
 
 export const getCourses = () => createAction({
-	endpoint: 'http://localhost:8000/api/courses/?limit=11000',
+	endpoint: `${ENDPOINT}/api/courses/?limit=11000`,
 	method: 'GET',
 	types: [
 		'GET_COURSES_REQUEST',
