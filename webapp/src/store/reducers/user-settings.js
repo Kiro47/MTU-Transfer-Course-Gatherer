3317de545ec1677e0da94aa0fb4c1ce7978e8b7e
Max Isom <code@maxisom.me>
2020-04-30 14:40:04 -0500

    Remove debug console.log statement

--- webapp/src/store/reducers/user-settings.js
+++ webapp/src/store/reducers/user-settings.js
@@ -1,6 +1,5 @@
 import {TOGGLE_THEME} from '../actions/user-settings';
 
-console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
 const initalState = {
   // Defaults to false (dark mode) since upon first load `theme` is undefined
   lightTheme: window.localStorage.getItem('theme') ? window.localStorage.getItem('theme') : !window.matchMedia('(prefers-color-scheme: dark)').matches
