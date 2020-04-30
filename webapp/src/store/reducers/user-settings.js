bd2ea4818736141ce05819c7154863179363ea47
Max Isom <code@maxisom.me>
2020-04-29 21:30:38 -0500

    Set default theme to OS preference

--- webapp/src/store/reducers/user-settings.js
+++ webapp/src/store/reducers/user-settings.js
@@ -1,8 +1,9 @@
 import {TOGGLE_THEME} from '../actions/user-settings';
 
+console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
 const initalState = {
   // Defaults to false (dark mode) since upon first load `theme` is undefined
-  lightTheme: window.localStorage.getItem('theme') === 'light'
+  lightTheme: window.localStorage.getItem('theme') ? window.localStorage.getItem('theme') : !window.matchMedia('(prefers-color-scheme: dark)').matches
 };
 
 export default function (state = initalState, action) {
