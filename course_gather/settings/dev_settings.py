371cfe9768e6095c61e09ae3f428ad91eccab0ca
Max Isom <hi@maxisom.me>
2020-05-16 17:16:50 -0500

    Fix Flake8 errors

--- course_gather/settings/dev_settings.py
+++ course_gather/settings/dev_settings.py
@@ -1,11 +1,12 @@
+import os
 from course_gather.settings.base import *  # noqa: F401, F403
 
-SECRET_KEY = os.getenv('SECRET_KEY', '')  # noqa: F405
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa: F405
     }
 }
--- course_gather/settings/prod_settings.py
+++ course_gather/settings/prod_settings.py
@@ -1,6 +1,7 @@
+import os
 from course_gather.settings.base import *  # noqa: F401, F403
 
-SECRET_KEY = os.getenv('SECRET_KEY', '')  # noqa: F405
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
