bf5644558ef84e6b55e0dac7c6af6bf46342d97a
Max Isom <hi@maxisom.me>
2020-05-30 21:07:17 -0500

    Cache API result for 5 minutes

--- course_gather/viewsets.py
+++ course_gather/viewsets.py
@@ -21,6 +21,8 @@ from course_gather.filters import (
 
 from rest_framework import viewsets
 from django_filters import rest_framework as filters
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 
 class StateViewSet(viewsets.ModelViewSet):
@@ -47,6 +49,11 @@ class CourseViewSet(viewsets.ModelViewSet):
                                            'transfer_course_state_code')
     filterset_class = CourseFilter
     filter_backends = (filters.DjangoFilterBackend,)
+
+    # Cache for 5 minutes
+    @method_decorator(cache_page(60 * 5))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
 
 class MTUCourseViewSet(viewsets.ModelViewSet):
