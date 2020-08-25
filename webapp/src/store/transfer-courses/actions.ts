76f3d7af590ee68d839679a2ca03d879425e0342
Max Isom <codetheweb@users.noreply.github.com>
2020-08-25 09:21:51 -0500

    Bump course API limit
    Co-authored-by: Blu3spirits <blu3spirits@gmail.com>

--- webapp/src/store/transfer-courses/actions.ts
+++ webapp/src/store/transfer-courses/actions.ts
@@ -3,7 +3,7 @@ import {ENDPOINT} from '../../lib/config';
 import {GET_TRANSFER_COURSES_REQUEST, GET_TRANSFER_COURSES_SUCCESS, GET_TRANSFER_COURSES_ERROR} from './types';
 
 export const getCourses = () => createAction({
-  endpoint: `${ENDPOINT}/api/courses/?limit=12000`,
+  endpoint: `${ENDPOINT}/api/courses/?limit=15000`,
   method: 'GET',
   types: [
     GET_TRANSFER_COURSES_REQUEST,
